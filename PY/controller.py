46657383877b34d984936c5aea2d03d740f8fefb
Katherine_20 <mob1911494@gmail.com>
2022-07-31 12:56:56 +0300

    calc

---
+++ PY/controller.py
@@ -0,0 +1,10 @@
+
+import model_min as m
+import view
+
+def button_click():
+    value_a = view.get_value()
+    value_b = view.get_value()
+    m.init(value_a,value_b)
+    result = m.do_it()
+    view.view_data('result',result)
---
+++ PY/view.py
@@ -0,0 +1,5 @@
+def view_data(title,data):
+    print(f'{title} = {data}')
+
+def get_value():
+    return int(input("value = "))
