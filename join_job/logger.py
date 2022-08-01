d20a24fcb544b22c98b54ad46c522f5692eb9270
Katherine_20 <mob1911494@gmail.com>
2022-08-01 09:10:39 +0300

    data temp, pressure, windspeed

---
+++ join_job/logger.py
@@ -0,0 +1,19 @@
+from datetime import datetime as dt
+from time import time as time
+def temperature_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.cvs','a') as file:
+        file.write('{};temperature;{}\n'
+                    .format(time, data))
+                   
+def pressure_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.cvs','a') as file:
+        file.write('{};pressure;{}\n'
+                    .format(time, data))
+                    
+def wind_speed_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.cvs','a') as file:
+        file.write('{};wind_speed;{}\n'
+                    .format(time, data))
---
+++ join_job/user_interface.py
@@ -0,0 +1,17 @@
+import data_provider as prov
+import logger as log
+
+def temperature_view(senson):
+    data = prov.get_temperature(senson)
+    log.temperature_logger(data)
+    return data
+
+def pressure_view(senson):
+    data = prov.get_pressure(senson)
+    log.pressure_logger(data)
+    return data
+
+def wind_speed_view(senson):
+    data = prov.get_wind_speed(senson)
+    log.wind_speed_logger(data)
+    return data
